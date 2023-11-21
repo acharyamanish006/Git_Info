3024f05635fe31d1b2df70c55429ab00a9d5bcf0
acharyamanish006 <manish.acharya313@gmail.com>
2023-11-21 22:49:50 +1100

    contribution flag added

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"time"
 	// "github.com/go-delve/delve/service/api"
 )
 
@@ -14,6 +15,7 @@ type GitInfo struct {
 	Name      string `json:"name"`
 	Url       string `json:"html_url"`
 	Bio       string `json:"bio"`
+	Location  string `json:"location"`
 	Followers int    `json:"followers"`
 	Following int    `json:"following"`
 	Repos     int    `json:"public_repos"`
@@ -48,7 +50,7 @@ func getUserInfo(api string) {
 
 	json.Unmarshal(body, &info)
 
-	fmt.Printf("Name: %s \nUrl: %s \nBio: %s \nRepos: %d \nFollowers: %d \nFollowing: %d  \n", info.Name, info.Url, info.Bio, info.Repos, info.Followers, info.Following)
+	fmt.Printf("Name: %s \nUrl: %s \nBio: %s \nLocation: %s \nRepos: %d \nFollowers: %d \nFollowing: %d  \n", info.Name, info.Url, info.Bio, info.Location, info.Repos, info.Followers, info.Following)
 }
 
 func getUserFollower(api string) {
@@ -95,6 +97,7 @@ func argsCommand() {
 	followerFlag := flag.Bool("F", false, "Specify the follower")
 	followingFlag := flag.Bool("f", false, "Specify the following")
 	help := flag.Bool("h", false, "Specify the repository name")
+	showContribution := flag.Bool("c", false, "contribution")
 
 	flag.Parse()
 
@@ -117,6 +120,8 @@ func argsCommand() {
 
 	case *help:
 		Help()
+	case *showContribution:
+		contributionChart(*userFlag)
 	default:
 		getUserInfo(api)
 	}
@@ -157,4 +162,94 @@ func Help() {
 	println("| --> go run main.go -u {git_username} -f                                                                            |")
 	println("|____________________________________________________________________________________________________________________|")
 
+}
+
+type GitHubEvent struct {
+	Type       string `json:"type"`
+	CreatedAt  string `json:"created_at"`
+	Repository struct {
+		Name string `json:"name"`
+	} `json:"repo"`
+}
+
+func contributionChart(username string) {
+	events, err := fetchGitHubEvents(username)
+	if err != nil {
+		fmt.Println("Error fetching GitHub events:", err)
+		return
+	}
+
+	contributions := getContributions(events)
+	displayContributionChart(contributions, 7)
+}
+
+func fetchGitHubEvents(username string) ([]GitHubEvent, error) {
+	url := fmt.Sprintf("https://api.github.com/users/%s/events", username)
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	var events []GitHubEvent
+	err = json.NewDecoder(response.Body).Decode(&events)
+	if err != nil {
+		return nil, err
+	}
+
+	return events, nil
+}
+
+func getContributions(events []GitHubEvent) []bool {
+	// Get the current day of the week
+	currentDay := int(time.Now().Weekday())
+
+	// Initialize contributions slice
+	contributions := make([]bool, 7)
+
+	// Update contributions based on GitHub events
+	for _, event := range events {
+		// Check if the event is a push event
+		if event.Type == "PushEvent" {
+			// Get the day of the week for the event
+			createdAt, err := time.Parse(time.RFC3339, event.CreatedAt)
+			if err == nil {
+				dayOfWeek := int(createdAt.Weekday())
+				// Update the contribution status for that day
+				contributions[(dayOfWeek-currentDay+7)%7] = true
+			}
+		}
+	}
+
+	return contributions
+}
+
+func displayContributionChart(contributions []bool, width int) {
+	// Display the contribution chart
+	fmt.Printf(" Contribution Chart \n\n")
+	fmt.Println(" __________________________________")
+	fmt.Println("|    |    |    |    |    |    |    |")
+	fmt.Println("| Mo | Tu | We | Th | Fr | Sa | Su |")
+	fmt.Println("|____|____|____|____|____|____|____|")
+
+	fmt.Println(" __________________________________")
+	fmt.Println("|    |    |    |    |    |    |    |")
+
+	for i := 0; i < len(contributions); i++ {
+		// Display the day of the week
+		// if i%7 == 0 {
+		// 	// fmt.Printf("\n")
+		// }
+
+		// Display the contribution status
+		if contributions[i] {
+			fmt.Print("| X  ")
+		} else {
+			fmt.Print("| _  ")
+		}
+	}
+	fmt.Print("|")
+	fmt.Println("\n|____|____|____|____|____|____|____|")
+
+	fmt.Println("\n\n X: Contribution,   Blank: No Contribution")
 }
