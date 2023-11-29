1330481d019c13ec63124b11551d4de9d16997da
acharyamanish006 <manish.acharya313@gmail.com>
2023-11-29 16:26:52 +1100

    some change

--- contributionChart.go
+++ contributionChart.go
@@ -11,17 +11,27 @@ import (
 
 func contributionChart(username string) {
 	re := lipgloss.NewRenderer(os.Stdout)
-	labelStyle := re.NewStyle().Foreground(lipgloss.Color("241"))
+	labelStyle := re.NewStyle().Foreground(lipgloss.Color("245"))
 
 	board := generateContributionBoard(7, 53)
 
 	t := table.New().
-		Border(lipgloss.NormalBorder()).
+		// Border(lipgloss.NormalBorder()).
+		BorderStyle(re.NewStyle().Foreground(lipgloss.Color("#91B39F"))).
 		BorderRow(true).
 		BorderColumn(true).
 		Rows(board...).
 		StyleFunc(func(row, col int) lipgloss.Style {
-			return lipgloss.NewStyle().Padding(0, 1)
+			even := row%2 == 0
+			if even {
+				return lipgloss.NewStyle().Padding(0, 1).Background(lipgloss.Color("#078203"))
+			} else if row%3 == 0 {
+				// Styling for rows divisible by 3
+				return lipgloss.NewStyle().Padding(0, 1).Background(lipgloss.Color("#034f01"))
+			} else {
+				return lipgloss.NewStyle().Padding(0, 1).Background(lipgloss.Color("#91B39F"))
+			}
+
 		})
 
 	months := labelStyle.Render(strings.Join([]string{"     January", "     February", "     March", "     April", "     May", "     June", "     July", "     August", "     September", "     October", "     November", "     December     "}, "  "))
